3855c2d88b5d6a12492d8f04ba88964e68c4eb32
Marcos Alarcon <86171565+alarconmarcos@users.noreply.github.com>
2022-08-24 15:20:52 -0300

    Add files via upload

---
+++ Resta1.~dp
@@ -0,0 +1,16 @@
+program resta1;
+
+uses
+  Forms,
+  resta11 in 'resta11.pas' {Form1},
+  resta12 in 'resta12.pas' {Form2};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.Title := 'Resta 1';
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
---
+++ Resta12.pas
@@ -0,0 +1,33 @@
+unit resta12;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.DFM}
+
+end.
